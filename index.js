004d093a8bb05b7d4a0cf842043f164c0cd6a204
mightyalex200 <quantumtraveling@gmail.com>
2017-01-22 17:16:59 -0500

    guard clauses

--- index.js
+++ index.js
@@ -55,71 +55,71 @@ class Bot {
                     break;
 
                 case "server":
-                    if (isadmin) {
-                        if (this.server) {
-                            message.reply(this.server.name);
-                        } else {
-                            message.reply("Not in server");
-                        }
+                    if (!isadmin)
+                      break;
+                    if (this.server) {
+                        message.reply(this.server.name);
+                    } else {
+                        message.reply("Not in server");
                     }
                     break;
 
                 case "channels":
-                    if (isadmin) {
-                        if (this.server) {
-                            message.reply(this.server.channels.array().toString().replace(/,/g, ", "));
-                        } else {
-                            message.reply("Not in server");
-                        }
+                    if (!isadmin)
+                      break;
+                    if (this.server) {
+                        message.reply(this.server.channels.array().toString().replace(/,/g, ", "));
+                    } else {
+                        message.reply("Not in server");
                     }
                     break;
 
                 case "channel":
-                    if (isadmin) {
-                        if (this.channel) {
-                            message.reply(this.channel.name + " in " + this.server.name);
-                        } else {
-                            message.reply("Not in channel");
-                        }
+                    if (!isadmin)
+                      break;
+                    if (this.channel) {
+                        message.reply(this.channel.name + " in " + this.server.name);
+                    } else {
+                        message.reply("Not in channel");
                     }
                     break;
 
                 case "joinserver":
-                    if (isadmin) {
-                        var jointext = message.content.slice(11);
-                        var tojoin = client.guilds.findAll('name', jointext)[0];
-                        if (tojoin) {
-                            message.reply("Joined server " + jointext);
-                            this.server = tojoin;
-                        } else {
-                            message.reply("Server not found: " + jointext)
-                        }
+                    if (!isadmin)
+                      break;
+                    var jointext = message.content.slice(11);
+                    var tojoin = client.guilds.findAll('name', jointext)[0];
+                    if (tojoin) {
+                        message.reply("Joined server " + jointext);
+                        this.server = tojoin;
+                    } else {
+                        message.reply("Server not found: " + jointext)
                     }
                     break;
 
                 case "joinchannel":
-                    if (isadmin) {
-                        if (this.server) {
-                            var jointext = message.content.slice(12).replace("#", "");
-                            var tojoin = this.server.channels.findAll('name', jointext)[0];
-                            if (tojoin) {
-                                message.reply("Joined channel " + jointext);
-                                this.channel = tojoin;
-                            } else {
-                                message.reply("Channel not found: " + jointext)
-                            }
+                    if (!isadmin)
+                      break;
+                    if (this.server) {
+                        var jointext = message.content.slice(12).replace("#", "");
+                        var tojoin = this.server.channels.findAll('name', jointext)[0];
+                        if (tojoin) {
+                            message.reply("Joined channel " + jointext);
+                            this.channel = tojoin;
+                        } else {
+                            message.reply("Channel not found: " + jointext)
                         }
                     }
                     break;
                 case "say":
-                    if (isadmin) {
-                        if (this.channel) {
-                            var saytext = message.content.slice(4);
-                            if (this.channel.type == "text") {
-                                this.channel.sendMessage(saytext);
-                            } else {
-                                message.reply(this.channel.type + " channel");
-                            }
+                    if (!isadmin)
+                      break;
+                    if (this.channel) {
+                        var saytext = message.content.slice(4);
+                        if (this.channel.type == "text") {
+                            this.channel.sendMessage(saytext);
+                        } else {
+                            message.reply(this.channel.type + " channel");
                         }
                     }
                     break;
